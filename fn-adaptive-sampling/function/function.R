b75012989a06f41911b0646726fe7d847a975bde
Jonathan Smith <jonathan@peoplesized.com>
2019-05-16 14:53:41 +0200

    more

--- fn-adaptive-sampling/function/function.R
+++ fn-adaptive-sampling/function/function.R
@@ -10,57 +10,41 @@ function(params) {
 
   candidates$uncertainty_prob <- candidates$uncertainty / sum(candidates$uncertainty)
   
-  in_sample <-
-    sample(1:nrow(candidates), 1, prob = candidates$uncertainty_prob)
-  samp_prob <-
-    samp_entropy_prob  <- candidates$uncertainty_prob[in_sample]
-  samp_entropy <- candidates$entropy[in_sample]
+  in_sample <- sample(1:nrow(candidates), 1, prob = candidates$uncertainty_prob)
   
-
   # Loop
   
   if (batch_size > 1) {
     for (i in 1:(batch_size - 1)) {
-      candiates_in_sample <- candidates[in_sample,]
-      candiates_not_in_sample <- candidates[-in_sample,]
+      candidates_in_sample <- candidates[in_sample,]
+      candidates_not_in_sample <- candidates[-in_sample,]
       
       # First calc distance between the in_sample and the rest
-      nn <-
-        nn2(candiates_in_sample[, c("lng", "lat")], candiates_not_in_sample[, c("lng", "lat")])
+      nn <- nn2(st_coordinates(candidates_in_sample), st_coordinates(candidates_not_in_sample))
       
       # convert distances to selection probability
       min_dist_to_other_points <- apply(nn$nn.dists, 1, min)
-      min_dist_to_other_points <-
-        min_dist_to_other_points / sum(min_dist_to_other_points)
-      
-      # Calc mean distance between each potential site and its 10 nearest neighbours
-      # mean_dist_to_nearest_neighbours <- nn2(candiates_not_in_sample[,c("lng", "lat")], candiates_not_in_sample[,c("lng", "lat")])$nn.dist[,2:10]
-      # mean_dist_to_nearest_neighbours_inv <- 1 / apply(mean_dist_to_nearest_neighbours, 1, mean)
-      # mean_dist_to_nearest_neighbours_inv <- mean_dist_to_nearest_neighbours_inv / sum(mean_dist_to_nearest_neighbours_inv)
+      min_dist_to_other_points <- min_dist_to_other_points / sum(min_dist_to_other_points)
       
       # Multiply by entropy
-      candiates_not_in_sample$pen_entropy <-
-        candiates_not_in_sample$entropy_prob * min_dist_to_other_points #* mean_dist_to_nearest_neighbours_inv
-      candiates_not_in_sample$pen_entropy_prob <-
-        candiates_not_in_sample$pen_entropy / sum(candiates_not_in_sample$pen_entropy)
-      
-      # Optional weight to add to probability sample
-      # candiates_not_in_sample$pen_entropy_prob <- candiates_not_in_sample$pen_entropy_prob^3 /
-      #  sum(candiates_not_in_sample$pen_entropy_prob^3)
+      candidates_not_in_sample$pen_entropy <-
+        candidates_not_in_sample$entropy_prob * min_dist_to_other_points #* mean_dist_to_nearest_neighbours_inv
+      candidates_not_in_sample$pen_entropy_prob <-
+        candidates_not_in_sample$pen_entropy / sum(candidates_not_in_sample$pen_entropy)
       
       # Sample
       entropy_sample <-
-        sample(1:nrow(candiates_not_in_sample),
+        sample(1:nrow(candidates_not_in_sample),
                1,
-               prob = candiates_not_in_sample$pen_entropy_prob)
-      next_site <- candiates_not_in_sample$id[entropy_sample]
+               prob = candidates_not_in_sample$pen_entropy_prob)
+      next_site <- candidates_not_in_sample$id[entropy_sample]
       samp_prob <-
-        c(samp_prob, candiates_not_in_sample$pen_entropy_prob[entropy_sample])
+        c(samp_prob, candidates_not_in_sample$pen_entropy_prob[entropy_sample])
       samp_entropy <-
-        c(samp_entropy, candiates_not_in_sample$entropy[entropy_sample])
+        c(samp_entropy, candidates_not_in_sample$entropy[entropy_sample])
       samp_entropy_prob <-
         c(samp_entropy_prob,
-          candiates_not_in_sample$entropy_prob[entropy_sample])
+          candidates_not_in_sample$entropy_prob[entropy_sample])
       in_sample <- c(in_sample, next_site)
     }
   }
