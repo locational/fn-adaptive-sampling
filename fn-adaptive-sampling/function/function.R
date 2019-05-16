a1e0548254803d679d867adee2fc69f4d0099f6c
Jonathan Smith <jonathan@peoplesized.com>
2019-05-16 15:56:30 +0200

    rewrote more

--- fn-adaptive-sampling/function/function.R
+++ fn-adaptive-sampling/function/function.R
@@ -1,18 +1,19 @@
 library(RANN)
+library(sf)
 
 function(params) {
   # 1. Handle input
   candidates_geojson = params[['point_data']]
-  batch_size = if is.null(params[['batch_size']]) 1 else params[['batch_size']]
+  batch_size = if (is.null(params[['batch_size']])) 1 else params[['batch_size']]
 
   # 2. Process
-  candidates = st_read(as.json(candidates_geojson)) # creates sf object
+  candidates = st_read(as.json(candidates_geojson), quiet=T) # creates sf object
   candidates$uncertainty_prob <- candidates$uncertainty / sum(candidates$uncertainty)
 
   in_sample <- sample(1:nrow(candidates), 1, prob = candidates$uncertainty_prob)
   
   candidates_in_sample <- candidates[in_sample,]
-  candidates_not_in_sample <- candidates[!in_sample,]
+  candidates_not_in_sample <- candidates[-in_sample,]
   # Loop
   
   if (batch_size > 1) {
@@ -29,12 +30,12 @@ function(params) {
       candidates_not_in_sample$pen_uncertainty <- 
         candidates_not_in_sample$uncertainty_prob * min_dist_to_other_points
       
-      candidates_not_in_sample$pen_uncertainty_prob <- 
+      candidates_not_in_sample$uncertainty_prob <- 
         candidates_not_in_sample$pen_uncertainty / sum(candidates_not_in_sample$pen_uncertainty)
       
       # Sample
-      uncertainty_sample <- sample(1:nrow(candidates_not_in_sample), 1, prob = candidates_not_in_sample$pen_uncertainty_prob)
-      candidates_in_sample <- rbind(candidates_in_sample, candidates_not_in_sample[uncertainty_sample,])
+      uncertainty_sample <- sample(1:nrow(candidates_not_in_sample), 1, prob = candidates_not_in_sample$uncertainty_prob)
+      candidates_in_sample <- rbind(candidates_in_sample, candidates_not_in_sample[uncertainty_sample,-"pen_uncertainty"])
       candidates_not_in_sample <- candidates_not_in_sample[-uncertainty_sample,]
     }
   }
